fb1c093b583350fceffdfe419e534e726109c467
Charles Junior <charlesjunnr@gmail.com>
2023-03-22 13:52:24 -0300

    REFATURADO - pela metade ainda...

--- BrandNewCalculator/Program.cs
+++ BrandNewCalculator/Program.cs
@@ -2,118 +2,146 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int MostrarMenu()
         {
-            string[] historico = new string[10];
+            Console.Clear();
+            Console.WriteLine("Brand New Calculator - 2023 ");
+            Console.WriteLine("--- Escolha a operação desejada -- ");
+            Console.WriteLine("1 - Adição.");
+            Console.WriteLine("2 - Subtração.");
+            Console.WriteLine("3 - Multiplicação.");
+            Console.WriteLine("4 - Divisão.");
+            Console.WriteLine("5 - Tabuada.");
+            Console.WriteLine("6 - Mostrar histórico de operações.");
+            Console.WriteLine("7 - Finalizar");
+            Console.WriteLine();
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            return escolha; // rertorna o valor da variável. É necessário criar uma variável no método Main para receber esse valor. 
+        }
+        static void MensagemInvalida(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensagem);
+            Console.ResetColor();
+
+        } // REUTILIZAÇÃO DO CÓDIGO NA MAIN
+        static void GerarTabuada() // NOMES DE FUNÇÕES SEMPRE COMEÇAM COM LETRA MAIÚSCULA;
+        {
+            Console.WriteLine("Digite um número para fazer a tabuada: ");
+            int numberT = Convert.ToInt32(Console.ReadLine());
+            int numberFt;
+            for (int i = 0; i <= 10; i++)
+            {
+                int resto = i % 2;
+                if (resto == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+
+                numberFt = numberT * i;
+                Console.WriteLine(numberT + " x " + i + " = " + numberFt);
+                Console.ResetColor();
+                
+            }
+            Console.ReadLine();
+        }
+        static void VisualizarOperacoesRealizadas(string[] historico) // No método MAIN foi criada uma variável desse tipo e foi passada por parâmetro.
+        {
+            for (int i = 0; i < historico.Length; i++)
+            {
+                if (historico[i] != null)
+                {
+                    Console.WriteLine(historico[i]);
+
+                }
+            }
+            Console.ReadLine();
+        }
+        static decimal ObterValorDecimal(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            decimal numero = Convert.ToDecimal(Console.ReadLine());
+            return numero;
+        }
+        static void Main(string[] args) //Funções tipo VOID não retornam valores. 
+        {
+            string[] historico = new string[100];
             int operacoesH = 0;
             decimal number1, number2, numberFinal;
-            int escolha;
             bool ehVerdade = true;
             do
             {
-                if (operacoesH == 10)
+                if (operacoesH == 100)
                 {
                     operacoesH = 0;
                 }
 
-                Console.Clear();
-                Console.WriteLine("Brand New Calculator - 2023 ");
-                Console.WriteLine("--- Escolha a operação desejada -- ");
-                Console.WriteLine("1 - Adição.");
-                Console.WriteLine("2 - Subtração.");
-                Console.WriteLine("3 - Multiplicação.");
-                Console.WriteLine("4 - Divisão.");
-                Console.WriteLine("5 - Tabuada.");
-                Console.WriteLine("6 - Mostrar histórico de operações.");
-                Console.WriteLine("7 - Finalizar");
-                Console.WriteLine();
+                int escolha = MostrarMenu(); // Variável criada para receber o valor da variável criada no Menu. 
 
-                escolha = Convert.ToInt32(Console.ReadLine());
-                Console.Clear();
+                if(escolha == 5)
+                {
+                    GerarTabuada();
+                    continue;
+                }else if (escolha == 6)
+                {
+                    VisualizarOperacoesRealizadas(historico);
+                    continue;
+                }else if(escolha == 7)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Obrigado por usar a calculadora!");
+                    ehVerdade = false;
+                }else if(
+                    escolha != 1 &&
+                    escolha != 2 &&
+                    escolha != 3 &&
+                    escolha != 4 &&
+                    escolha != 5 &&
+                    escolha != 6 &&
+                    escolha != 7)
+                {
+                    MensagemInvalida("Opção não encontrada no menu! Digite novamente!");
+                    Console.ReadLine();
+                    continue;
+                }
+                number1 = ObterValorDecimal("Digite o valor do primeiro número: ");
+                number2 = ObterValorDecimal("Digite o valor do segundo número: ");
 
                 switch (escolha)
                 {
                     case 1:
-                        Console.WriteLine("Digite o primeiro número: ");
-                        number1 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("Digite o segundo número: ");
-                        number2 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("\n");
                         numberFinal = number1 + number2;
                         Console.WriteLine("O resultado é " + number1 + " + " + number2 + " = " + numberFinal);
                         historico[operacoesH] = number1 + " + " + number2 + " = " + numberFinal;
 
                         break;
                     case 2:
-                        Console.WriteLine("Digite o primeiro número: ");
-                        number1 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("Digite o segundo número: ");
-                        number2 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("\n");
                         numberFinal = number1 - number2;
                         Console.WriteLine("O resultado é " + number1 + " - " + number2 + " = " + numberFinal);
                         historico[operacoesH] = number1 + " - " + number2 + " = " + numberFinal;
                         break;
                     case 3:
-                        Console.WriteLine("Digite o primeiro número: ");
-                        number1 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("Digite o segundo número: ");
-                        number2 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("\n");
                         numberFinal = number1 * number2;
                         Console.WriteLine("O resultado é " + number1 + " * " + number2 + " = " + numberFinal);
                         historico[operacoesH] = number1 + " * " + number2 + " = " + numberFinal;
-
                         break;
                     case 4:
-                        Console.WriteLine("Digite o primeiro número: ");
-                        number1 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("Digite o segundo número: ");
-                        number2 = Convert.ToDecimal(Console.ReadLine());
-                        Console.WriteLine("\n");
                         if (number2 == 0)
-                        {
-                            Console.WriteLine("\n");
-                            Console.WriteLine("Esse número é inválido! Escreva novamente o segundo número: ");
-                            number2 = Convert.ToDecimal(Console.ReadLine());
-                        }
+                            {
+                                MensagemInvalida("O segundo número não pode ser ZERO!");
+                                Console.WriteLine("Escreva novamente o segundo número: ");
+                                number2 = Convert.ToDecimal(Console.ReadLine());
+                            }
+
                         numberFinal = number1 / number2;
+
                         Console.WriteLine("O resultado é " + number1 + " / " + number2 + " = " + numberFinal);
                         historico[operacoesH] = number1 + " / " + number2 + " = " + numberFinal;
                         break;
-
-                    case 5:
-                        Console.WriteLine("Digite um número para fazer a tabuada: ");
-                        int numberT = Convert.ToInt32(Console.ReadLine());
-                        int numberFt;
-                        for (int i = 0; i <= 10; i++)
-                        {
-                            numberFt = numberT * i;
-                            Console.WriteLine(numberT + " x " + i + " = " + numberFt);
-                        }
-                        break;
-
-                    case 6:
-                        for (int i = 0; i < historico.Length; i++)
-                        {
-                            if (historico[i] != null)
-                            {
-                                Console.WriteLine(historico[i]);
-
-                            }
-                        }
-                        Console.ReadLine();
-                        break;
-                    case 7:
-                        Console.WriteLine("\n");
-                        Console.WriteLine("Obrigado por usar a calculadora!");
-                        ehVerdade = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Essa opção é inválida! Digite novamente!");
-                        break;
-
                 }
                 operacoesH++;
                 Console.ReadLine();
